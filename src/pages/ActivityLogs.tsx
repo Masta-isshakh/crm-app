d89cd4f1849904e8f1dfb9987a10d8aa5eae0dc4
Masta-isshakh <mastaisshakh@gmail.com>
2025-12-15 04:13:53 -0800

    my first code

--- src/pages/ActivityLogs.tsx
+++ src/pages/ActivityLogs.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "../../amplify/data/resource";
-import "./activity-log.css";
+import "./activity.css";
 
 const client = generateClient<Schema>();
 
