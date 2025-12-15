b087c9be0bd454aa0c8d12d0263b03b0df817f19
Masta-isshakh <mastaisshakh@gmail.com>
2025-12-15 00:11:27 -0800

    my first code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-// src/App.tsx
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import MainLayout from "./components/MainLayout";
 
-
 export default function App() {
-return (
-<Authenticator>
-{({ signOut, user }) => (
-<MainLayout user={user} signOut={signOut || (() => {})} />
-)}
-</Authenticator>
-);
+  return (
+    <Authenticator>
+      {() => <AppContent />}
+    </Authenticator>
+  );
+}
+
+function AppContent() {
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
+  
+  return <MainLayout user={user || null} signOut={signOut || (() => {})} />;
 }
